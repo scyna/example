cc732b11834298d1eee12174ce938933738f14a1
NGUYEN Hong San <hongsan@gmail.com>
2023-02-08 08:32:18 +0700

    go/hello example

---
+++ go/hello/service/define.go
@@ -0,0 +1,12 @@
+package service
+
+import scyna "github.com/scyna/core"
+
+const (
+	HELLO_URL = "/example/hello/hello"
+	ADD_URL   = "/example/hello/add"
+)
+
+var (
+	ADD_RESULT_TOO_BIG = scyna.NewError(100, "Too Big")
+)
