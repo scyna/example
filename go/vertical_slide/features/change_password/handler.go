62011018e8b643ee5df4aeb94b531a2e7b7bf4ec
NGUYEN Hong San <hongsan@gmail.com>
2023-02-21 20:15:54 +0700

    begin work on change password

--- go/vertical_slide/features/change_password/handler.go
+++ go/vertical_slide/features/change_password/handler.go
@@ -1,1 +1,17 @@
 package change_password
+
+import (
+	"ex/vertical_slide/shared/proto"
+
+	scyna "github.com/scyna/core"
+)
+
+const URL = "/account/create"
+
+func Handler(ctx *scyna.Endpoint, request *proto.ChangePasswordRequest) scyna.Error {
+	ctx.Info("Receive ChangePasswordRequest")
+
+	/*TODO*/
+
+	return nil
+}
---
+++ go/vertical_slide/features/change_password/repository.go
@@ -0,0 +1,23 @@
+package change_password
+
+import (
+	"ex/vertical_slide/shared/model"
+	"ex/vertical_slide/shared/repository"
+
+	scyna "github.com/scyna/core"
+)
+
+type Repository struct {
+	repository.BaseRepository
+}
+
+func NewRepository(LOG *scyna.Logger) *Repository {
+	return &Repository{
+		BaseRepository: *repository.NewBaseRepository(LOG),
+	}
+}
+
+func (r *Repository) ChangePasword(account *model.Account, password string) scyna.Error {
+
+	return nil
+}
